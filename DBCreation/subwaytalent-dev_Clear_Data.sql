9ea398aeddcbaa5dbb663264ecd8ac5d7db4f7e0
jomgithub <jeromecorpuz84@gmail.com>
2017-05-17 12:40:59 +0800

    added clear data

---
+++ DBCreation/subwaytalent-dev_Clear_Data.sql
@@ -0,0 +1,18 @@
+
+delete from event_invites;
+delete from event_planner;
+delete from event_planner_feedbacks;
+delete from event_preferred_genre;
+delete from event_preferred_skills;
+delete from payment_exceptions;
+delete from planner_payments;
+delete from talent_genres;
+delete from talent_skills;
+delete from user_login;
+delete from user_files;
+delete from user_devices;
+delete from user_notifications;
+delete from user_external_media;
+delete from user_tokens;
+delete from user_account;
+delete from `subwaytalent-prod`.event;
